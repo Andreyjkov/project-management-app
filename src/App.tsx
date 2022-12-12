0806eb0c3f377a1f6fad1c32fc8ff6c45ff67c9a
Andreyjkov <Andreyjskov@gmail.com>
2022-12-12 21:05:08 +0300

    feat: add route for static page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Boards } from './pages/Boards';
+import { NotFound } from './pages/NotFound';
+import { Profile } from './pages/Profile';
+import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
+import { Welcome } from './pages/Welcome';
 
 function App() {
-  return <h1>work</h1>;
+  const isLoggedIn = true;
+  return (
+    <Routes>
+      <Route path="/" element={isLoggedIn ? <Welcome /> : <Navigate to="/signin" />} />
+      <Route path="/boards" element={<Boards />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Profile() {
+  return <h1>Profile page</h1>;
+}
