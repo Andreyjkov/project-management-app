e3465e20bae5948eb0a0e0e6edb8508f87a9960f
Andreyjkov <Andreyjskov@gmail.com>
2022-12-12 23:33:37 +0300

    fix: export page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Boards } from './pages/Boards';
-import { NotFound } from './pages/NotFound';
-import { Profile } from './pages/Profile';
-import { SignIn } from './pages/SignIn';
-import { SignUp } from './pages/SignUp';
-import { Welcome } from './pages/Welcome';
+import {
+  Boards, NotFound, Profile, SignIn, SignUp, Welcome,
+} from './pages';
+import store from './store';
 
 function App() {
   const isLoggedIn = true;
   return (
-    <Routes>
-      <Route path="/" element={isLoggedIn ? <Welcome /> : <Navigate to="/signin" />} />
-      <Route path="/boards" element={<Boards />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <Welcome /> : <Navigate to="/signin" />} />
+        <Route path="/boards" element={<Boards />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </Provider>
   );
 }
 
